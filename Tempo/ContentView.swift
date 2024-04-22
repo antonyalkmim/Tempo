399b38ea5001853fb810f0afdb56fe394b75a2c7
Antony Alkmim <antony-alkmim@hotmail.com>
2024-04-22 12:48:42 -0300

    Initial Commit

---
+++ Tempo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tempo
+//
+//  Created by Antony Nelson Daudt Alkmin on 22/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Antony Nelson Daudt Alkmin on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
